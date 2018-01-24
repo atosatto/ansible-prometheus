e0ce38f10cf5654ad5defb6df26fb2bd3e8b7833
Andrea Tosatto <andrea.tosy@gmail.com>
2018-01-24 23:43:53 +0100

    Add more tests covering custom Prometheus configuration.

---
+++ molecule/prometheus-config/tests/test_prometheus-config.py
@@ -0,0 +1,25 @@
+import os
+import re
+import json
+import urllib2
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_prometheus_custom_config(host):
+
+    f = host.file('/etc/prometheus/prometheus.yml')
+
+    # we check that the scrape_interval variables is now set to 1m
+    assert f.contains('scrape_interval: 1m')
+    # we check that the evaluation_interval is left to 15s as defined in vars/main.yml
+    assert f.contains('evaluation_interval: 15s')
+    # we check that the new variable called 'scrape_timeout' as been added to the global configuration
+    assert f.contains('scrape_timeout: 10s')
+    # we check that the 'prometheus' job is defined
+    assert f.contains('job_name: "prometheus"')
+    # we check that the 'node_exporter' job is defined
+    assert f.contains('job_name: "node_exporter"')
