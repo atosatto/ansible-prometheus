96ebad96f76bf839d475e759ae0a36faf50a3516
Andrea Tosatto <andrea.tosy@gmail.com>
2019-02-01 21:38:34 +0100

    CI Ansible versions upgrade (#3)

--- molecule/prometheus-latest/tests/test_prometheus-latest.py
+++ molecule/prometheus-latest/tests/test_prometheus-latest.py
@@ -7,7 +7,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-gh = Github(os.getenv('TRAVIS_GH_TOKEN', None))
+gh = Github(os.getenv('GITHUB_API_TOKEN', None))
 prom_last_release = re.sub('^v(.*)$', '\\1', gh.get_repo('prometheus/prometheus').get_latest_release().tag_name)
 prom_last_artifact = "prometheus-" + prom_last_release + ".linux-amd64"
 
